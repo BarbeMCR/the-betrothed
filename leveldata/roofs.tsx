5c974bad8ece2a868e5d518b0be96582b46e100a
BarbeMCR <74430134+BarbeMCR@users.noreply.github.com>
2022-06-02 19:16:56 +0200

    Added 0.07 files

---
+++ leveldata/roofs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="roofs" tilewidth="64" tileheight="64" tilecount="20" columns="5">
+ <image source="../assets/level/ground/roofs.png" width="320" height="256"/>
+</tileset>
---
+++ leveldata/trees.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="trees" tilewidth="256" tileheight="256" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="../assets/level/ground/tree.png"/>
+ </tile>
+</tileset>
