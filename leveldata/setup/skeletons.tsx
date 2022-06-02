e7bb21d165fa9bb510b1e91835ee80ff851624e7
BarbeMCR <74430134+BarbeMCR@users.noreply.github.com>
2022-06-02 19:14:42 +0200

    Deleted leveldata folder to avoid duplicates

--- leveldata/setup/skeletons.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="skeletons" tilewidth="64" tileheight="64" tilecount="2" columns="2">
- <image source="../../assets/level/skeletonsetup.png" width="128" height="64"/>
-</tileset>
