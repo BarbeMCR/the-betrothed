8abb28b86fee837fb956ba50573969fb5b5e98c9
BarbeMCR <74430134+BarbeMCR@users.noreply.github.com>
2022-06-04 17:35:26 +0200

    Deleted duplicates

--- leveldata/bg_day.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="bg_day" tilewidth="64" tileheight="64" tilecount="256" columns="16">
- <image source="../assets/level/ground/bg_day.png" width="1024" height="1024"/>
-</tileset>
--- leveldata/setup/enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="skeletons" tilewidth="64" tileheight="64" tilecount="2" columns="2">
- <image source="../../assets/level/enemysetup.png" width="128" height="64"/>
-</tileset>
