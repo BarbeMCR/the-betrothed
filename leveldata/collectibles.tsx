6c3be1184eb611de8e71a8fbdebcc9a603b965d2
BarbeMCR <74430134+BarbeMCR@users.noreply.github.com>
2022-06-04 17:37:24 +0200

    Added 0.08 files

---
+++ leveldata/collectibles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="collectibles" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../assets/level/energy/blue/energyblue1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../assets/level/energy/red/energyred1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../assets/level/energy/yellow/energyyellow1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../assets/level/energy/green/energygreen1.png"/>
+ </tile>
+</tileset>
